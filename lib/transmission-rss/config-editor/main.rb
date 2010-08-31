24d657f320dbc8fdc86529363c6f3169d5aa4616
henning mueller <henning@orgizm.net>
2010-08-31 00:40:26 +0000

    more options in gui, connected gui to config object, renamed update_interval and start_paused

--- lib/transmission-rss/config-editor/main.rb
+++ lib/transmission-rss/config-editor/main.rb
@@ -20,9 +20,19 @@ class TransmissionRSS::ConfigEditor
 			Gtk.main_quit
 		end
 
-		@entry1.activates_default = true
+		@entry_feed_url.activates_default = true
 
+		initialize_config
 		initialize_listbox
+	end
+
+	def initialize_config
+		@entry_server_host.text = @config.server.host
+		@entry_server_port.text = @config.server.port.to_s
+
+		@entry_update_interval.text = @config.update_interval.to_s
+
+		@checkbutton_add_paused.active = @config.start_paused
 	end
 
 	def initialize_listbox
@@ -54,6 +64,17 @@ class TransmissionRSS::ConfigEditor
 
 	def on_button_remove_feed( widget )
 		@listbox.remove( @entry1.text )
+	end
+
+	def on_button_save( widget )
+		@config.server.host = @entry_server_host.text
+		@config.server.port = @entry_server_port.text.to_i
+
+		@config.update_interval = @entry_update_interval.text.to_i
+
+		@config.start_paused = @checkbutton_add_paused.active?
+
+		p @config
 	end
 
 	def on_menu_quit( widget )
