12e45c837bff86c65707cc229349b5fb9d6c2724
henning mueller <mail@nning.io>
2017-10-28 21:33:28 +0200

    Refactor Log

--- lib/transmission-rss/log.rb
+++ lib/transmission-rss/log.rb
@@ -6,21 +6,30 @@ module TransmissionRSS
   class Log
     include Singleton
 
-    # Change log target (IO or path to a file as String).
-    def target=(target)
+    def initialize(target = $stderr)
       old_logger = @logger
-      @logger = Logger.new target
+      @logger ||= Logger.new(target)
 
       if old_logger
         @logger.level = old_logger.level
         @logger.formatter = old_logger.formatter
+      else
+        @logger.level = Logger::DEBUG
+        @logger.formatter = proc do |sev, time, _, msg|
+          time = time.strftime('%Y-%m-%d %H:%M:%S')
+          "#{time} (#{sev.downcase}) #{msg}\n"
+        end
       end
+    end
+
+    # Change log target (IO or path to a file as String).
+    def target=(target)
+      initialize(target)
     end
 
     # If this class misses a method, call it on the encapsulated Logger class.
     def method_missing(sym, *args)
-      @logger ||= Logger.new $stderr
-      @logger.send sym, *args
+      @logger.send(sym, *args)
     end
   end
 end
