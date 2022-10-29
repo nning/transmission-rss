50f10b849002d8e4e2093d73daf31c5d4aec9c7f
Daniel Smith <danny@stormpoopersmith.com>
2022-10-29 01:31:54 +0100

    Basic aggregator seen tests

--- spec/aggregator_spec.rb
+++ spec/aggregator_spec.rb
@@ -55,38 +55,47 @@ describe Aggregator do
   describe '#process_link' do
     before(:each) do    
       VCR.use_cassette('feed_fetch', MATCH_REQUESTS_ON) do  
-        ITEM = subject.send(:parse, subject.send(:fetch, FEEDS.first)).first
+        @item = subject.send(:parse, subject.send(:fetch, FEEDS.first)).first
       end
+      subject.seen.clear!
     end
 
-    it 'returns enclosure link' do
-      content = subject.send(:process_link, FEEDS.first, ITEM)
+    it 'returns enclosure link and adds to seen' do
+      content = subject.send(:process_link, FEEDS.first, @item)
 
       url = URI.parse(content)
 
       expect(url.scheme).to eq('https')
       expect(url.host).to eq('www.archlinux.org')
       expect(File.basename(url.path)).to match(/\.iso\.torrent$/)
+      
+      expect(subject.seen.size).to eq(1)
+      expect(subject.seen.include?(content)).to be true
     end
 
-    it 'returns link if no enclosure link' do
-      ITEM.enclosure = nil
+    it 'returns link and adds to seen if no enclosure link' do
+      @item.enclosure = nil
       
-      content = subject.send(:process_link, FEEDS.first, ITEM)
+      content = subject.send(:process_link, FEEDS.first, @item)
 
       url = URI.parse(content)
       expect(url.scheme).to eq('https')
       expect(url.host).to eq('www.archlinux.org')
       expect(File.basename(url.path)).to match(/2020\.01\.01$/)
+      
+      expect(subject.seen.size).to eq(1)
+      expect(subject.seen.include?(content)).to be true
     end
 
     it 'returns nil if no link or enclosure link' do
-      ITEM.enclosure = nil
-      ITEM.link = nil
+      @item.enclosure = nil
+      @item.link = nil
       
-      content = subject.send(:process_link, FEEDS.first, ITEM)
+      content = subject.send(:process_link, FEEDS.first, @item)
 
       expect(content).to be_nil
+      
+      expect(subject.seen.size).to eq(0)
     end
   end
 end
